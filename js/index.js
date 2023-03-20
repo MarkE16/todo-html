56d9aba30e6a51aa530e54e5b54f299cdacc24d2
Mark <markevola24@gmail.com>
2023-03-19 20:26:05 -0500

    working on custom checkbox

--- js/index.js
+++ js/index.js
@@ -17,9 +17,9 @@ input.addEventListener("input", function(event) {
 function objectMap(obj) {
   return Object.keys(obj).map(key => {
     return {
-      text: obj[key].text,
-      checked: obj[key].checked
-    }
+      ...obj[key],
+      id: key
+    };
   });
 }
 
@@ -29,6 +29,38 @@ window.onload = function() {
   mappedItems.forEach(el => {
     addTodo(el);
   });
+}
+
+/**
+ *
+ */
+
+
+
+/**
+ * This function is primarily used to call array-like methods on array-like objects like
+ * `HTMLCollection` and `NodeList` where they do not have these methods.
+ * @param {string} method - The method to call on the array-like object.
+ * @param {any[]} arr - The array-like object.
+ * @param {function(any): any | null} callback - The callback function to pass to the method.
+ * @returns {any[]} The new array with the method applied.
+ */
+function arrayMethod(method, arr, callback) {
+  /*
+    Since we're working with HTMLCollection, we need to call
+    `Array.prototype.filter.call()` to use the filter method, because
+    if we try to use the normal `.filter()` method, it will throw an error.
+   */
+  switch (method) {
+    case "filter":
+      return Array.prototype.filter.call(arr, callback);
+    case "map":
+      return Array.prototype.map.call(arr, callback);
+    case "find":
+      return Array.prototype.find.call(arr, callback);
+    default:
+      return arr;
+  }
 }
 
 function updateTodo(e) {
@@ -73,7 +105,9 @@ function getCompleted() {
   if (all.length === 0) {
     counter.textContent = "+ No Todos!";
   } else {
-    const complete = [...all].filter(el => el.children[0].checked).length;
+    const complete = arrayMethod("filter", all, el => {
+      return el.children[0].children[0].checked;
+    }).length;
     if (complete === all.length) {
       counter.textContent = "+ Hooray! Finished with everything!";
     } else {
@@ -96,13 +130,23 @@ function clearTodos() {
 }
 
 function save() {
-  const items = [...all].map(el => {
+  const items = arrayMethod("map", all, el => {
     return {
       text: el.children[1].textContent,
-      checked: el.children[0].checked
-    }
-  })
+      checked: el.children[0].children[0].checked
+    };
+  });
   localStorage.setItem("todos", JSON.stringify(items));
+}
+
+/**
+ * Like the name suggests, this function inserts a node after another node.
+ * @param {Node} newNode - The HTML node to insert.
+ * @param {Node} referenceNode - The HTML node to insert the new node after.
+ * @returns {void}
+ */
+function insertAfter(newNode, referenceNode) {
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
 // Giant block of code !!
@@ -114,25 +158,29 @@ function addTodo(objVal=undefined) {
   const delBtn = document.createElement("button");
   const editBtn = document.createElement("button");
   const cancelBtn = document.createElement("button");
+  const checkboxSpan = document.createElement("span");
   const lbl = document.createElement("label");
+  const textSpan = document.createElement("label");
   const editInput = document.createElement("input");
   let isEditing = false;
 
   // Edit attributes for the nodes.
   if (objVal) {
-    lbl.textContent = objVal.text;
+    textSpan.textContent = objVal.text;
     checkbox.checked = objVal.checked;
     editInput.value = objVal.text;
     if (checkbox.checked) {
       lbl.style.textDecoration = "line-through";
     }
   } else {
-    lbl.textContent = input.value;
+    textSpan.textContent = input.value;
     editInput.value = input.value;
   }
   // Not the best way to create a unique ID, but it works for the time being.
   const randomID = Math.floor(Math.random() * new Date().getTime());
+  textSpan.htmlFor = "todo-" + randomID;
   lbl.htmlFor = "todo-" + randomID;
+  lbl.tabIndex = 0;
   checkbox.id = "todo-" + randomID;
   currTodo.id = "" + randomID;
   delBtn.textContent = "Delete";
@@ -143,16 +191,18 @@ function addTodo(objVal=undefined) {
   editInput.type = "text";
   checkbox.type = "checkbox";
   editBtn.className = "action-btn";
-  delBtn.className = "action-btn";
-  cancelBtn.className = "action-btn";
+  delBtn.className = "action-btn del";
+  cancelBtn.className = "action-btn cancel";
   editInput.className = "edit-input";
   editInput.placeholder = "Enter Todo";
   currTodo.className = "todo";
+  checkboxSpan.className = "checkbox";
+  lbl.className = "checkbox-container";
 
   // `onclick` events for buttons & inputs
   delBtn.onclick = function() {
-    if ((!checkbox.checked && window.confirm(`'${lbl.textContent}' is not complete. Delete anyway?`)) || checkbox.checked) {
-      const selected = [...todos.children].find(el => el.id === currTodo.id);
+    if ((!checkbox.checked && window.confirm(`'${textSpan.textContent}' is not complete. Delete anyway?`)) || checkbox.checked) {
+      const selected = arrayMethod("find", todos.children, el => el.id === currTodo.id);
       todos.removeChild(selected);
       if (all.length === 0) {
         disable(clearBtn);
@@ -165,7 +215,7 @@ function addTodo(objVal=undefined) {
   editBtn.onclick = function() {
     if (!isEditing) {
       editBtn.textContent = "Finish";
-      hide(lbl);
+      hide(textSpan);
       show(editInput);
       isEditing = true;
       editInput.focus();
@@ -177,10 +227,10 @@ function addTodo(objVal=undefined) {
         editInput.focus();
         return;
       }
-      lbl.textContent = editInput.value;
+      textSpan.textContent = editInput.value;
       editBtn.textContent = "Edit";
-      show(lbl);
       hide(editInput);
+      show(textSpan);
       isEditing = false;
       delBtn.style.display = "none";
       cancelBtn.style.display = "none";
@@ -189,7 +239,7 @@ function addTodo(objVal=undefined) {
   }
 
   checkbox.onclick = function() {
-    const selected = [...todos.children].find(el => el.id === currTodo.id).children[1];
+    const selected = arrayMethod("find", todos.children, el => el.id === currTodo.id).children[1];
     if (checkbox.checked) {
       selected.style.textDecoration = "line-through";
     } else {
@@ -207,10 +257,10 @@ function addTodo(objVal=undefined) {
 
   cancelBtn.onclick = function() {
     hide(editInput);
-    show(lbl);
+    show(textSpan);
     editBtn.textContent = "Edit";
     isEditing = false;
-    editInput.value = lbl.textContent;
+    editInput.value = textSpan.textContent;
     delBtn.style.display = "none";
     cancelBtn.style.display = "none";
   }
@@ -218,8 +268,11 @@ function addTodo(objVal=undefined) {
   // Adding nodes to the DOM and small changes to other nodes.
   fragment
     .appendChild(currTodo)
+    .appendChild(lbl)
     .appendChild(checkbox)
-    .after(lbl)
+    .after(checkboxSpan)
+  // insertAfter(textSpan, checkboxSpan);
+  currTodo.appendChild(textSpan);
   currTodo.appendChild(editInput);
   currTodo.appendChild(editBtn);
   currTodo.appendChild(delBtn);
